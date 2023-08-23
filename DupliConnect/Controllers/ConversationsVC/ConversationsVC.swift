bac25a494cd058e1123d5d3ec83aa2d1bf4fe46c
Mehmet Can Şimşek <mehmetsimsek_98@icloud.com>
2023-08-23 13:04:51 +0300

    app settings adjusted

--- DupliConnect/Controllers/ConversationsVC/ConversationsVC.swift
+++ DupliConnect/Controllers/ConversationsVC/ConversationsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsVC.swift
 //  DupliConnect
 //
 //  Created by Mehmet Can Şimşek on 23.08.2023.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
-
 }
-
