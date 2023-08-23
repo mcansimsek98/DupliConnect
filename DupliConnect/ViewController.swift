aeb3cf394f98678c987e9971089996ac7dc663e7
Mehmet Can Şimşek <mehmetsimsek_98@icloud.com>
2023-08-23 12:50:06 +0300

    Initial Commit

---
+++ DupliConnect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DupliConnect
+//
+//  Created by Mehmet Can Şimşek on 23.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
